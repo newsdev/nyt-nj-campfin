c172f4e2458ad9eba3bfaa0c1016cc3e3ab7c912
Gautam Hathi <gautamhathi@live.com>
2017-08-10 18:26:42 -0400

    adding docs to njcampfin scraper

--- njcampfin/__init__.py
+++ njcampfin/__init__.py
@@ -25,6 +25,9 @@ client = boto3.client(
 )
 
 def check_exists_by_id(id, driver):
+    '''
+    Checks if an element with input id exists on a webpage
+    '''
     try:
         driver.find_element_by_id(id)
     except NoSuchElementException:
@@ -32,6 +35,9 @@ def check_exists_by_id(id, driver):
     return True
 
 def check_exists_by_xpath(xpath, driver):
+    '''
+    Checks if an element with input xpath exists on a webpage
+    '''
     try:
         driver.find_element_by_xpath(xpath)
     except NoSuchElementException:
@@ -39,13 +45,18 @@ def check_exists_by_xpath(xpath, driver):
     return True
 
 def find_next_page_button_index(elements):
+    '''
+    Finds the button to go to the next page of search results
+    '''
     for i in range(len(elements)):
         if elements[i].text == ' > ':
             return i
     return -1
 
 def write_data(out, outfile):
-    
+    '''
+    Outputs object to a file
+    '''
     with open(outfile, 'w') as f:
         f.write(json.dumps(out))
     print("Results written")
@@ -53,10 +64,16 @@ def write_data(out, outfile):
     return out
 
 def upload_json_to_s3(output_json, bucket, folder, filename):
+    '''
+    Uploads scraped data to S3
+    '''
     write_data(output_json, 'nj_campfin_scraped.json')
     client.upload_file('nj_campfin_scraped.json', bucket, folder + filename)
 
 def get_previously_scraped_from_s3(output_filename):
+    '''
+    Gets previously scraped data from S3 file
+    '''
     url = os.environ['AWS_URL_PREFIX'] + '/' + os.environ['BUCKET_NAME'] + '/' + os.environ['FOLDER_PATH'] + output_filename
     data = []
     try:
@@ -67,6 +84,9 @@ def get_previously_scraped_from_s3(output_filename):
     return data
 
 def create_filing_name_from_json(filing_json):
+    '''
+    Creates file name for pdf from a scraped filing
+    '''
     name = ''.join(x for x in filing_json['name'] if x.isalnum())
     date = ''.join(x for x in filing_json['date'] if x.isalnum())
     form = ''.join(x for x in filing_json['form'] if x.isalnum())
@@ -77,6 +97,9 @@ def create_filing_name_from_json(filing_json):
     return out
 
 def get_filing_and_upload_to_s3(url, referer_url, bucket, folder, filename):
+    '''
+    Downloads a filing pdf and then uploads it to S3
+    '''
     reply = get(url, stream=True, headers={'Referer': referer_url})
     with open('temp_filing.pdf', 'wb') as file:
         for chunk in reply.iter_content(chunk_size=1024):
@@ -88,6 +111,9 @@ def get_filing_and_upload_to_s3(url, referer_url, bucket, folder, filename):
     return file_url
 
 def advance_to_page(browser, page_controls_xpath, page_num):
+    '''
+    Goes to a certain page of filing results
+    '''
     while True:
         if check_exists_by_xpath(page_controls_xpath, browser):
             page_controls = browser.find_elements_by_xpath(page_controls_xpath)
@@ -106,6 +132,9 @@ def advance_to_page(browser, page_controls_xpath, page_num):
             page_controls[advance_several].click()
 
 def convert_csv_row_to_result(csv_row):
+    '''
+    Takes a row from the logged results and converts it to a scraped object
+    '''
     if len(csv_row) > 7:
         return {
             'name': csv_row[0],
@@ -133,15 +162,23 @@ def convert_csv_row_to_result(csv_row):
         }
 
 def get_filing_list(first_name, last_name, year, office, outfile, get_filings, upload_pdfs, logfile):
+    '''
+    Scrapes filings with inputted search parameters and outputs them with given output parameters
+
+    TODO: separate input and output portions into different functions? 
+    '''
     print("Began scraping with params {} {} {} {} {}".format(first_name, last_name, year, office, outfile))
 
-    results = []
+    results = [] # stores scraping results
+
+    # read in scraped results if logfile is inputted
     if logfile is not None and logfile != '':
         reader = csv.reader(logfile)
         for row in reader:
             results.append(convert_csv_row_to_result(row))
         print(len(results))
 
+    # read in results previous scraping passes if previously_scraped file is inputted
     previously_scraped = []
     if outfile is not None and outfile != '':
         previously_scraped = get_previously_scraped_from_s3(outfile.split('/')[-1])
@@ -153,23 +190,26 @@ def get_filing_list(first_name, last_name, year, office, outfile, get_filings, u
             del copy['url']
         previously_scraped_no_urls.append(copy)
 
+    # set up webdriver
     #path_to_chromedriver = '/usr/local/bin/chromedriver' #TODO -> env var
     path_to_chromedriver = os.environ['PATH_TO_CHROMEDRIVER']
     browser = webdriver.Chrome(executable_path = path_to_chromedriver)
 
+    # open filings search page
     url = 'http://www.elec.state.nj.us/ELECReport/searchcandidate.aspx' #TODO -> env var, maybe?
-
     browser.get(url)
 
+    # "type" search parameters into form
     browser.find_element_by_id('txtFirstName').send_keys(first_name)
     browser.find_element_by_id('txtLastName').send_keys(last_name)
     if (year is not None and year != ''):
         browser.find_element_by_xpath("//select[@name='ctl00$ContentPlaceHolder1$usrCandidate1$ddlYear']/option[text()='{}']".format(year)).click()
     if (office is not None and office != ''):
         browser.find_element_by_xpath("//select[@name='ctl00$ContentPlaceHolder1$usrCandidate1$ddlOffice']/option[text()='{}']".format(office)).click()
+    
+    browser.find_element_by_id('btnSearch').click() # search
 
-    browser.find_element_by_id('btnSearch').click()
-
+    # xpaths to find things on the search results page
     docs_table_xpath =  "//div[@id='VisibleReportContentctl00_ContentPlaceHolder1_BITSReportViewer1_reportViewer1_ctl09']//table[@cols='6']"
     docs_table_or_norecords_xpath = "//div[@id='VisibleReportContentctl00_ContentPlaceHolder1_BITSReportViewer1_reportViewer1_ctl09'][.//table[@cols='6'] or .//div/text()='No Records Found']"
     names_table_xpath = "//table[@id='ContentPlaceHolder1_usrCommonGrid1_gvwData']"
@@ -177,30 +217,40 @@ def get_filing_list(first_name, last_name, year, office, outfile, get_filings, u
     page_controls_xpath = "//a[@class='bodytext']"
     date_sort_controls_xpath = "//td/a[@tabindex]"
 
+    # get the page controls on the page
     if check_exists_by_xpath(page_controls_xpath, browser):
         page_controls = browser.find_elements_by_xpath(page_controls_xpath)
     else:
         page_controls = None
 
+    # advance to results that haven't been scraped if there are logged scraped results
     if len(results) > 25:
         advance_to_page(browser, page_controls_xpath, (len(results) // 25) + 1)
     if logfile is not None and logfile != '':
         writer = csv.writer(logfile)
-
+    
+    # scraping loop
+    # scrapes each page of search results, then advances to next page
+    # breaks if no more pages 
     while True:
+        # wait for search results to show up
         wait = WebDriverWait(browser, int(os.environ['WAIT_TIME']))
         wait.until(
             EC.presence_of_element_located((By.XPATH, docs_table_or_norecords_xpath))
         )
 
+        # get the list of candidates on the results page
         names_table = browser.find_element_by_xpath(names_table_xpath)
         names_rows = names_table.find_elements_by_xpath("./tbody/tr")
 
+        # for each candidate...
         for i in range(1, len(names_rows)):
+            # get the list of candidates on the results page
             names_table = browser.find_element_by_xpath(names_table_xpath)
             names_rows = names_table.find_elements_by_xpath("./tbody/tr")
             name_row = names_rows[i]
 
+            # get the name and information in the name row element
             name = name_row.find_element_by_xpath("./td/a").text
             name_items = name_row.find_elements_by_xpath("./td")
             location = name_items[1].text
@@ -209,12 +259,14 @@ def get_filing_list(first_name, last_name, year, office, outfile, get_filings, u
             election_type = name_items[4].text
             year = name_items[5].text
 
+            # click on the candidate name
             try:
                 name_row.click()
             except WebDriverException:
                 print(" ".join([name, location, party, office_or_type, election_type, year]) + " FAILED")
                 continue
 
+            # wait for results to show up
             try:
                 wait = WebDriverWait(browser, int(os.environ['WAIT_TIME']))
                 wait.until(
@@ -223,18 +275,23 @@ def get_filing_list(first_name, last_name, year, office, outfile, get_filings, u
             except TimeoutException:
                 print("TIMEOUT EXCEPTION: " + name)
 
+            # if there are no filings, continue
             if (check_exists_by_xpath("//div[@id='VisibleReportContentctl00_ContentPlaceHolder1_BITSReportViewer1_reportViewer1_ctl09']//div[text()='No Records Found']", browser)):
                 print("No records, continuing")
                 if get_filings:
                     continue
-
+            
+            # get the candidate contributors url
             if check_exists_by_xpath("//a[text()='Summary Data']", browser):
                 summary_data_link = browser.find_element_by_xpath("//a[text()='Summary Data']").get_attribute("href")
             else:
                 summary_data_link = ''
 
+            # if you need to scrape the filings (and not just the candidate info)...
             if get_filings:
-                browser.find_element_by_xpath(date_sort_controls_xpath).click()
+                browser.find_element_by_xpath(date_sort_controls_xpath).click() # sort the filings by date
+                
+                # wait for sort to complete and loading screen to go away
                 wait = WebDriverWait(browser, int(os.environ['WAIT_TIME']))
                 wait.until(EC.presence_of_element_located((By.XPATH, "//img[@src='/ELECReport/Reserved.ReportViewerWebControl.axd?OpType=Resource&Version=12.0.2402.20&Name=Microsoft.ReportingServices.Rendering.HtmlRenderer.RendererResources.sortAsc.gif']")))
                 wait = WebDriverWait(browser, int(os.environ['WAIT_TIME']))
@@ -243,7 +300,9 @@ def get_filing_list(first_name, last_name, year, office, outfile, get_filings, u
                     EC.invisibility_of_element_located((By.ID, "ctl00_ContentPlaceHolder1_BITSReportViewer1_reportViewer1_AsyncWait"))
                     #EC.element_to_be_clickable((By.XPATH, date_sort_controls_xpath))
                 )
-                browser.find_element_by_xpath(date_sort_controls_xpath).click()
+                browser.find_element_by_xpath(date_sort_controls_xpath).click() # click sort again to sort the right way
+                
+                # wait...
                 wait = WebDriverWait(browser, int(os.environ['WAIT_TIME']))
                 wait.until(
                     EC.presence_of_element_located((By.XPATH, "//img[@src='/ELECReport/Reserved.ReportViewerWebControl.axd?OpType=Resource&Version=12.0.2402.20&Name=Microsoft.ReportingServices.Rendering.HtmlRenderer.RendererResources.sortDesc.gif']"))
@@ -254,9 +313,13 @@ def get_filing_list(first_name, last_name, year, office, outfile, get_filings, u
                     EC.invisibility_of_element_located((By.ID, "ctl00_ContentPlaceHolder1_BITSReportViewer1_reportViewer1_AsyncWait"))
                 )
 
-                docs_table = browser.find_element_by_xpath(docs_table_xpath)
-                filing_rows = docs_table.find_elements_by_xpath("./tbody/tr")
+                docs_table = browser.find_element_by_xpath(docs_table_xpath) #get the filings table
+                filing_rows = docs_table.find_elements_by_xpath("./tbody/tr") #get rows in the filings table
+                
+                #for each row in the filing table...
                 for filing_row in filing_rows[2:]:
+
+                    # scrape the info in the row
                     filing_items = filing_row.find_elements_by_xpath(".//div")
                     file_element = filing_row.find_element_by_xpath(".//a")
                     details = {
@@ -273,6 +336,7 @@ def get_filing_list(first_name, last_name, year, office, outfile, get_filings, u
                         'amendment' : filing_items[3].text.strip(),
                     }
 
+                    # get url for filing and download it if input options say you should
                     if outfile is not None and outfile != '' and details not in previously_scraped:
                         if details in previously_scraped_no_urls:
                             prev_details_index = previously_scraped_no_urls.index(details)
@@ -288,6 +352,7 @@ def get_filing_list(first_name, last_name, year, office, outfile, get_filings, u
                     else:
                         details['url'] = ''
                     
+                    # write scraped data to log if logfile is given
                     if details not in results and logfile is not None and logfile != '':
                         writer.writerow([
                             details['name'],
@@ -306,7 +371,7 @@ def get_filing_list(first_name, last_name, year, office, outfile, get_filings, u
                         logfile.flush()
                     if details not in results:
                         results.append(details)
-            else:
+            else: # abbreviated writeout for just candidate info
                 details = {
                     'name': name.strip(),
                     'summary_link': summary_data_link,
@@ -331,6 +396,7 @@ def get_filing_list(first_name, last_name, year, office, outfile, get_filings, u
                 if details not in results:
                     results.append(details)
         
+        # advance to next filings page, if it exists, else break
         if check_exists_by_xpath(page_controls_xpath, browser):
             page_controls = browser.find_elements_by_xpath(page_controls_xpath)
         else:
@@ -341,6 +407,7 @@ def get_filing_list(first_name, last_name, year, office, outfile, get_filings, u
         else:
             page_controls[find_next_page_button_index(page_controls)].click()
     
+    # write out results to outfile or stdout
     if outfile is not None and outfile != '':
         write_data(results, outfile)
         upload_json_to_s3(results, bucket, folder, outfile.split('/')[-1])
@@ -350,6 +417,8 @@ def get_filing_list(first_name, last_name, year, office, outfile, get_filings, u
 def main():
     '''with open('log.csv', 'r+') as logfile:
     get_filing_list(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6].lower() == 'true', sys.argv[7].lower() == 'true', None)'''
+    # run the scraping job every 6 hours
+    # TODO: move scraping interval to environment var
     schedule.every(6).hours.do(get_filing_list, sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[7].lower() == 'true', sys.argv[7].lower() == 'true', None)
     while True:
         schedule.run_pending()
